87992ceb32ca83d87908766592cfadf8db1894c1
flowers <172275766@qq.com>
2018-11-10 23:08:03 +0800

    '静态页面'

---
+++ src-logo/components/HelloVue.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="">
+<h2>{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        msg:'哈哈'
+      }
+    }
+  }
+</script>
+<style>
+h2{
+  color: red;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Flowers博爵 on 2018/11/10.
+ */
+/*入口JS文件*/
+import Vue from 'vue'
+import App from './App.vue';
+import './index.css';
+new Vue({
+  el:'#app',
+  components:{
+    App
+  },
+  template:'<App/>'
+})
